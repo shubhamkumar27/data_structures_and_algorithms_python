b992387ebd9c4d84f4247e2317791079bc64d137
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-08-21 22:36:51 +0530

    BST

--- Trees/Binary_trees.py
+++ Trees/Binary_trees.py
@@ -8,8 +8,34 @@ class Binarytrees():
     def __init__(self):
         self.root = None
 
-    def insert(self, value):
-        node = Node(value)
-        if (self.root==None):
-            self.root = node
-            
+    def insert(self,arr):
+        self.root = self.insert1(arr=arr)
+
+    def insert1(self,arr, root=None, i = 0):
+        n=len(arr)
+        if i < n:
+            temp = Node(arr[i])
+            root = temp
+            # insert left child
+            root.left = self.insert1(arr, root.left, 2 * i + 1)
+            # insert right child
+            root.right = self.insert1(arr, root.right, 2 * i + 2)
+        return root
+
+    def preorder(self,root = None):
+        if self.root == None:
+            return
+        if root == None:
+            root = self.root
+        while(root):
+            print(root.data)
+            if (root.left):
+                self.preorder(root.left)
+            if (root.right):
+                self.preorder(root.right)
+            break
+
+l = [1,2,3,4,5,6,7,8,9]
+tree = Binarytrees()
+tree.insert(l)
+tree.preorder()
