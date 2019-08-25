6d4c8646fd2aa9aaef7d4bc39a3f3d038edd17f0
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-08-25 15:43:34 +0530

    trees

---
+++ Trees/lowest_common_ancestor.py
@@ -0,0 +1,45 @@
+class Node():
+    def __init__(self, value):
+        self.data = value
+        self.left = None
+        self.right = None
+
+class BST():
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        node = Node(value)
+        root = self.root
+        if root==None:
+            self.root = node
+            return
+        while(True):
+            if(value<=root.data):
+                if(root.left == None):
+                    root.left = node
+                    break
+                else:
+                    root = root.left
+            else:
+                if(root.right == None):
+                    root.right = node
+                    break
+                else:
+                    root = root.right
+
+    def lca(self, root, v1, v2):
+        if (root.data > v1 and root.data > v2):
+            root = self.lca(root.left, v1, v2)
+        if (root.data < v1 and root.data < v2):
+            root = self.lca(root.right, v1, v2)
+        return root.data
+
+tree = BST()
+tree.insert(7)
+tree.insert(4)
+tree.insert(9)
+tree.insert(8)
+tree.insert(10)
+tree.insert(2)
+print(tree.lca(tree.root,2,10))
