0a3b396b5a4ac268168ce0d3a2beb40f1b6b10a5
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-09-02 22:40:15 +0530

    graph

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -1,1 +1,19 @@
-#### code for graph data structures ####
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.edges = []
+
+class Edge(object):
+    def __init__(self, value, node_from, node_to):
+        self.value = value
+        self.node_from = node_from
+        self.node_to = node_to
+
+class Graph(object):
+    def __init__(self, nodes=[], edges=[]):
+        self.nodes = nodes
+        self.edges = edges
+
+    def insert_node(self, new_node_val):
+        new_node = Node(new_node_val)
+        self.nodes.append(new_node)
