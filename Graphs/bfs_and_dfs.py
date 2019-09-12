0002140eb8d88f9a6ca4e11b4c864957ac857d4f
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-09-12 20:43:08 +0530

    traversal

---
+++ Graphs/bfs_and_dfs.py
@@ -0,0 +1,32 @@
+from graph import Graph
+
+
+def bfs(graph, node):
+    snode = None
+    for nod in graph.nodes:
+        if nod.value == node:
+            snode = nod
+    if snode == None:
+        return print('Node not found')
+    v = []
+    q = [snode]
+    while q:
+        n = q.pop(0)
+        if n not in v:
+            v.append(n)
+            print(n.value)
+            for nnode in n.edges:
+                if nnode.node_to not in v:
+                    q.append(nnode.node_to)
+    return v
+
+
+graph = Graph()
+graph.insert_edge(100, 'B', 'A')
+graph.insert_edge(101, 'B', 'D')
+graph.insert_edge(102, 'B', 'E')
+graph.insert_edge(103, 'A', 'C')
+graph.insert_edge(103, 'A', 'F')
+graph.insert_edge(103, 'C', 'F')
+graph.insert_edge(103, 'F', 'E')
+bfs(graph, 'A')
--- Graphs/graph.py
+++ Graphs/graph.py
@@ -45,28 +45,27 @@ class Graph(object):
             if nod.value == node:
                 snode = nod
         if snode == None:
-            return 'Node not found'
+            return print('Node not found')
         v = []
         q = [snode]
         while q:
             n = q.pop(0)
             if n not in v:
                 v.append(n)
+                print(n.value)
                 for nnode in n.edges:
                     if nnode.node_to not in v:
                         q.append(nnode.node_to)
-        return v
 
-graph = Graph()
-graph.insert_edge(100, 'B', 'A')
-graph.insert_edge(101, 'B', 'D')
-graph.insert_edge(102, 'B', 'E')
-graph.insert_edge(103, 'A', 'C')
-graph.insert_edge(103, 'A', 'F')
-graph.insert_edge(103, 'C', 'F')
-graph.insert_edge(103, 'F', 'E')
-l = graph.bfs('B')
-for i in l:
-    print(i.value)
+# graph = Graph()
+# graph.insert_edge(100, 'B', 'A')
+# graph.insert_edge(101, 'B', 'D')
+# graph.insert_edge(102, 'B', 'E')
+# graph.insert_edge(103, 'A', 'C')
+# graph.insert_edge(103, 'A', 'F')
+# graph.insert_edge(103, 'C', 'F')
+# graph.insert_edge(103, 'F', 'E')
+# graph.bfs('B')
+
 
 
