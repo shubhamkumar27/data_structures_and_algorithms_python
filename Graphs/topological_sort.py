22bdb09721f98f33e1891cc65b97958276a0d3c9
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-09-14 11:57:08 +0530

    topological sort

---
+++ Graphs/topological_sort.py
@@ -0,0 +1,30 @@
+from graph import Graph
+
+def topological_sort(graph):
+    visited=[]
+    stack=[]
+    for i in graph.nodes:
+        if i not in visited:
+            topological_util(i,graph,visited,stack)
+    return stack
+
+
+def topological_util(j,graph,vis,stack):
+    vis.append(j)
+    for edge in j.edges:
+        if edge.node_to not in vis:
+            topological_util(edge.node_to,graph,vis,stack)
+    stack.insert(0,j)
+
+
+graph = Graph()
+graph.insert_edge(100, 5, 0)
+graph.insert_edge(101, 5, 2)
+graph.insert_edge(102, 2, 3)
+graph.insert_edge(103, 3, 1)
+graph.insert_edge(103, 4, 0)
+graph.insert_edge(103, 4, 1)
+#graph.insert_edge(103, 'F', 'E')
+lis = topological_sort(graph)
+for i in lis:
+    print(i.value)
