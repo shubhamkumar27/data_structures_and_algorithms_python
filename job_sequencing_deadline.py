e2656ed591bd50c5bb11c40d73a267e9a88ffce8
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-09-18 10:56:58 +0530

    job_sequencing

--- job_sequencing_deadline.py
+++ job_sequencing_deadline.py
@@ -1,9 +1,25 @@
+
+def sequencing(arr):
+    #### sorting to get the max no. of job to schedule
+    arr.sort(key=lambda x: x[1], reverse=True)
+    t = arr[0][1]
+    #### sorting jobs according to profit in decreasing order
+    arr.sort(key=lambda x: x[2], reverse=True)
+    slots=[False]*t
+    jobs=['empty']*t
+    for i in range(len(arr)):
+        for j in range(min(t-1,arr[i][1]-1),-1,-1):
+            if slots[j]==False:
+                slots[j]=True
+                jobs[j]=arr[i][0]
+                break
+    return jobs
+
+
 arr = [['a', 2, 100],
        ['b', 1, 19],
        ['c', 2, 27],
        ['d', 1, 25],
        ['e', 3, 15]]
 
-arr.sort(key=lambda x:x[2],reverse=True)
-
-print(arr)
+print(sequencing(arr))
