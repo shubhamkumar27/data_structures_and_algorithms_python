1cb49fefe7e188aac52fb188f6af43f9cf33aec3
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-09-21 14:58:10 +0530

    finding the missing number

--- finding_missing_number.py
+++ finding_missing_number.py
@@ -2,14 +2,29 @@
 # A list of n-1 integers will be given and they will be in range 1 to n
 # Find the missing number in list
 
-######### USING SUM METHOD ##############3
+######### USING SUM METHOD ##############
 
 def sum_method(lis):
     n = len(lis)
-    sum = (n+1)*(n+2)//2
+    summ = (n+1)*(n+2)//2
     for i in lis:
-        sum-=i
-    print('Missing number is : '+str(sum))
+        summ-=i
+    print('Missing number is : '+str(summ))
+
+######## USING XOR METHOD ###############
+
+def xor_method(lis):
+    x = lis[0]
+    for i in lis[1:]:
+        x = x^i
+
+    y = 1
+    for j in range(2,len(lis)+2):
+        y = y^j
+
+    z = x^y
+    print('Missing number is : '+str(z))
 
 l = [1,2,3,4,7,6,9,8]
 sum_method(l)
+xor_method(l)
