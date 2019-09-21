a3acc4547bd4c6a37da67f5b57452268dca12b1c
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-09-21 14:35:36 +0530

    printing matrix in spiral form

--- printing_matrix_in_spiral_form.py
+++ printing_matrix_in_spiral_form.py
@@ -3,21 +3,25 @@ def spiral(mat):
     c = len(mat[0])
     k = 0
     l = 0
-    for i in range(l,c):
-        print(mat[k][i])
-    k+=1
 
-    for i in range(k,r):
-        print(mat[i][r])
-    c-=1
+    while(k<r-1 and l<c-1):
+        for i in range(l,c):
+            print(mat[k][i])
+        k+=1
 
-    if(r>k):
-        for i in range(c-1,-1,-1):
-            print(mat[r-1][i])
-        r-=1
+        for i in range(k,r):
+            print(mat[i][r])
+        c-=1
 
-    if(c>l):
-        for i in range()
+        if(r>k):
+            for i in range(c-1,l-1,-1):
+                print(mat[r-1][i])
+            r-=1
+
+        if(c>l):
+            for i in range(r-1,k-1,-1):
+                print(mat[i][l])
+            l+=1
 
 
 matrix = [[1,2,3,4,5],
