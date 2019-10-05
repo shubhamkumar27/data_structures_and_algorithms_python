3f4df13a975bac1d38223d3d6ae33252e4d82ed4
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-05 12:22:31 +0530

    matrix rotation

--- matrix_rotation_by_90_degree.py
+++ matrix_rotation_by_90_degree.py
@@ -1,12 +1,20 @@
 def rotation(mat):
     n = len(mat)
-    for i in range(n-1):
-        mat[0][i],mat[0][n-1]=mat[0][n-1],mat[0][i]
-        mat[0][i],mat[n-1][n-1]=mat[n-1][n-1],mat[0][i]
-        mat[0][i],mat[n-1][0]=mat[n-1][0],mat[0][i]
+    level = n//2
+    l=0
+    while(l<level):
+        for i in range(l,n-1):
+            mat[l][i],mat[i][n-1]=mat[i][n-1],mat[l][i]
+            mat[l][i],mat[n-1][n-1-i+l]=mat[n-1][n-1-i+l],mat[l][i]
+            mat[l][i],mat[n-1-i+l][l]=mat[n-1-i+l][l],mat[l][i]
+        l+=1
+        n-=1
+
     print(mat)
 
-mat = [[1,2],
-       [3,4]]
+mat = [[1,2,5,10],
+       [3,4,6,11],
+       [7,8,9,12],
+       [13,14,15,16]]
 
 rotation(mat)
