ad3c3da55f10e2e5c6a84a144832c7400b02f548
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-08 15:20:33 +0530

    merging sorted linked list in reverse order completed

--- Linked lists/merging_sorted_list_in_reverse.py
+++ Linked lists/merging_sorted_list_in_reverse.py
@@ -26,3 +26,46 @@ class Linkedlist():
             print(cur.data,end=' ')
             cur = cur.next
         print()
+
+def merge_reverse(l1,l2):
+    if l1==None and l2==None: return None
+    t1 = l1.head
+    t2 = l2.head
+    result = None
+    while(t1 != None and t2 != None):
+        if t1.data <= t2.data:
+            temp = t1.next
+            t1.next = result
+            result = t1
+            t1 = temp
+        else:
+            temp = t2.next
+            t2.next = result
+            result = t2
+            t2 = temp
+    while(t1 != None):
+        temp = t1.next
+        t1.next = result
+        result = t1
+        t1 = temp
+    while(t2 != None):
+        temp = t2.next
+        t2.next = result
+        result = t2
+        t2 = temp
+    r = Linkedlist()
+    r.head = result
+    r.traverse()
+
+l1 = Linkedlist()
+l2 = Linkedlist()
+l1.insert(1)
+l1.insert(2)
+l1.insert(3)
+l1.insert(5)
+l1.insert(7)
+l1.insert(9)
+l2.insert(0)
+l2.insert(4)
+l2.insert(6)
+merge_reverse(l1,l2)
