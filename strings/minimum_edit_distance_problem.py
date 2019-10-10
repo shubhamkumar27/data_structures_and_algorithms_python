d892b26c29e2869720ab0193ec990865f245a65f
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-10 13:57:08 +0530

    Minimum edit distance problem

--- strings/minimum_edit_distance_problem.py
+++ strings/minimum_edit_distance_problem.py
@@ -12,4 +12,10 @@ def min_edit(s1,s2):
     if s1[m-1]==s2[n-1]:
         return min_edit(s1[:m-1],s2[:n-1])
 
+    else:
+        return 1 + min(min_edit(s1[:m],s2[:n-1]),min_edit(s1[:m-1],s2[:n]),min_edit(s1[:m-1],s2[:n-1]))
+
+s1 = 'shubham'
+s2 = ''
+print(min_edit(s1,s2))
     
