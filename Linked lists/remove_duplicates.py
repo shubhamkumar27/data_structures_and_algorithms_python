38e237f1eaaf7875e0df48aa918bc93bf23628df
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-12 22:29:34 +0530

    Remove duplicates

--- Linked lists/remove_duplicates.py
+++ Linked lists/remove_duplicates.py
@@ -26,3 +26,23 @@ class Linkedlist():
             print(cur.data,end=' ')
             cur = cur.next
         print()
+
+def duplicates(lis):
+    hashtable = dict()
+    temp = lis.head
+    while(temp):
+        if hashtable[str(temp.data)==True]:
+            pass
+    print(hashtable)
+
+lis = Linkedlist()
+lis.insert_end(1)
+lis.insert_end(2)
+lis.insert_end(3)
+lis.insert_end(4)
+lis.insert_end(5)
+lis.insert_end(6)
+lis.insert_end(7)
+lis.insert_end(8)
+lis.insert_end(9)
+duplicates(lis)
