c8e8846149a90a341593aa21040f122cd3ab3fba
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-16 22:32:15 +0530

    Leetcode

--- Leetcode/Q3-Longest Substring Without Repeating Characters.py
+++ Leetcode/Q3-Longest Substring Without Repeating Characters.py
@@ -17,3 +17,23 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3. 
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 '''
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        maxm = 0
+        l=0
+        hashtable = [0]*256
+        for i in range(len(s)):
+            has = ord(s[i])
+            if hashtable[has] != 1:
+                l+=1
+                hashtable[has] = 1
+            else:
+                l=0
+                hashtable[has] = 0
+            if l>maxm:
+                maxm=l
+        return maxm
