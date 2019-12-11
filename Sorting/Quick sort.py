6feac3a3ba8169a52a7218f89abd77d43a262989
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-12-12 00:02:19 +0530

    Sorting Algo

--- Sorting/Quick sort.py
+++ Sorting/Quick sort.py
@@ -1,10 +1,20 @@
 def quicksort(arr,start,end):
     if start<end:
-        pivot = partition(arr,start,end)
-        quicksort(arr,start,pivot-1)
-        quicksort(arr,pivot+1,end)
+        pivot_index = partition(arr,start,end)
+        quicksort(arr,start,pivot_index-1)
+        quicksort(arr,pivot_index+1,end)
+    return arr
 
 
 def partition(arr,start,end):
-    p = arr[end]
-    return p
+    pivot = arr[end]
+    pivot_index = start
+    for i in range(start,end):
+        if arr[i]<=pivot:
+            arr[i],arr[pivot_index] = arr[pivot_index], arr[i]
+            pivot_index+=1
+    arr[end], arr[pivot_index] = arr[pivot_index], arr[end]      
+    return pivot_index
+
+l = [2,5,8,6,1,0,9]
+print(quicksort(l,0,len(l)-1))
